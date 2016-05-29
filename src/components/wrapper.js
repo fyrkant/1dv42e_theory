d94888b660261b3778b7b3b614dcb0d9c4df6f13
Mattias <mw222rs@student.lnu.se>
2016-05-29 20:00:39 +0200

    bootstrapped react app, always takes TOO LONG

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -1,7 +1,12 @@
 import React from "react";
+import {Link} from "react-router";
 
 export default ({children}) => (
   <div>
     <h1>LABORATION</h1>
+    <div>
+      <Link to="list" >List</Link>
+      <Link to="list2">List 2</Link>
+    </div>
     <div>{children}</div>
   </div>);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import {render} from "react-dom";
-import {Router, hashHistory} from "react-router";
+import {IndexRoute, Router, Route, hashHistory} from "react-router";
 
-import routes from "./routes";
+import Wrapper from "./components/wrapper";
+import Welcome from "./components/welcome";
+import List from "./components/list";
+import List2 from "./components/list2";
 
 render(
   <Router
     history={hashHistory}
-    router={routes}
-  />,
+  >
+    <Route path="/" component={Wrapper}>
+      <IndexRoute component={Welcome} />
+      <Route path="list" component={List} />
+      <Route path="list2" component={List2} />
+    </Route>
+  </Router>,
   document.getElementById("root")
 );
