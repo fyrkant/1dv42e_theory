9c1c27409bd0e9f46dd5e534284d986c0c84b27b
Mattias <mw222rs@student.lnu.se>
2016-06-01 08:59:28 +0200

    welcome page text

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default () => {
-  return <p>Welcome!</p>;
+  return (
+    <div>
+      <h2>Welcome!</h2>
+      <p>This is a demonstration application for a presentation of my theory essay in the course 1dv42e at the Linnaeus University.</p>
+      <p>Click the hamburg menu up to the left and check out the examples.</p>
+    </div>
+  );
 };
