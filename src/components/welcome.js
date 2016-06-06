4c5d451030d0b00a864377781353ff38d17c2652
Mattias <mw222rs@student.lnu.se>
2016-06-06 15:12:58 +0200

    welcome page

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,7 +4,7 @@ export default () => {
   return (
     <div>
       <h2>Welcome!</h2>
-      <p>This is a demonstration application for a presentation of my theory essay in the course 1dv42e at the Linnaeus University.</p>
+      <p>This is a demonstration application for my theory essay in the course 1dv42e at the Linnaeus University.</p>
       <p>Click the hamburg menu up to the left and check out the examples.</p>
     </div>
   );
